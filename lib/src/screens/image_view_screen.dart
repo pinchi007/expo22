40ec97c7aba9be0582e3c8d491c1733ef4c901ab
pinchi007 <emmanuelguillen9214@gmail.com>
2023-10-27 09:30:56 -0600

    agregar funcion photo view

--- lib/src/screens/image_view_screen.dart
+++ lib/src/screens/image_view_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
 
+//agregando las funciones para photo view
 class ImageViewScreen extends StatelessWidget {
   final File imageFile;
 
